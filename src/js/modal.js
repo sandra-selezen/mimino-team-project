182f56b4ea5d137ee0d650abf4f777f8189274f1
AntonPetrenko <paa2017sh@gmail.com>
2022-12-12 22:44:40 +0200

    Chenged script
    Changed the script and corresponding classes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,44 +1,70 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-book-table]'),
-    closeModalBtn: document.querySelector('[data-modal-close-book-table]'),
-    modal: document.querySelector('[data-modal-book-table]'),
-  };
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+document.addEventListener('DOMContentLoaded', function() {
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-book-room]'),
-    closeModalBtn: document.querySelector('[data-modal-close-book-room]'),
-    modal: document.querySelector('[data-modal-book-room]'),
-  };
+   /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+   var modalButtons = document.querySelectorAll('.js-open-modal'),
+       overlay      = document.querySelector('.js-overlay-modal'),
+       closeButtons = document.querySelectorAll('.js-modal-close');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+   /* Перебираем массив кнопок */
+   modalButtons.forEach(function(item){
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-room-price]'),
-    closeModalBtn: document.querySelector('[data-modal-close-room-price]'),
-    modal: document.querySelector('[data-modal-room-price]'),
-  };
+      /* Назначаем каждой кнопке обработчик клика */
+      item.addEventListener('click', function(e) {
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+         e.preventDefault();
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+
+         /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+         modalElem.classList.add('active');
+         overlay.classList.add('active');
+      }); // end click
+
+   }); // end foreach
+
+
+   closeButtons.forEach(function(item){
+
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.modal');
+
+         parentModal.classList.remove('active');
+         overlay.classList.remove('active');
+      });
+
+   }); // end foreach
+
+
+    document.body.addEventListener('keyup', function (e) {
+        var key = e.keyCode;
+
+        if (key == 27) {
+
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        };
+    }, false);
+
+
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+
+
+
+
+}); // end ready
