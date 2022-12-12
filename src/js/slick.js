17fe9cede1e5172d042cc2b2871d59052d52e353
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2022-12-12 14:22:41 +0200

    add adaptive

--- src/js/slick.js
+++ src/js/slick.js
@@ -6,3 +6,12 @@ $('.hotel-slider').slick({
   adaptiveHeight: true,
   variableWidth: true,
 });
+
+$('.hotel-slider-right').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  variableWidth: true,
+});
