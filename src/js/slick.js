239240ecc0d12f4c7918d7b4866ec659cc03d680
vasylenkodi <vasylenko1daniil@gmail.com>
2022-12-12 17:04:56 +0100

    add styles for slider on about-restaurant section

--- src/js/slick.js
+++ src/js/slick.js
@@ -15,3 +15,13 @@ $('.hotel-slider-right').slick({
   adaptiveHeight: true,
   variableWidth: true,
 });
+
+
+$('.homepage-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  variableWidth: true,
+});
