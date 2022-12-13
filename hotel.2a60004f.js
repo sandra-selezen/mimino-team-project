bc45ac6873a657be9f71e35245aee0b2932c35a2
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2022-12-13 10:17:48 +0000

    Deploying to gh-pages from @ sandra-selezen/mimino-team-project@b2a500c30d5d3b682a47829fdecce1551679f896 🚀

--- hotel.2a60004f.js
+++
@@ -1,2 +0,0 @@
-$(".hotel-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0}),$(".hotel-slider-right").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0});
-//# sourceMappingURL=hotel.2a60004f.js.map
