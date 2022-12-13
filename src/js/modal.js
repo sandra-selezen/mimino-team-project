102e5d7cb04bf71d92f0b11cfb4389f642dffe0d
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2022-12-13 14:38:27 +0200

    connected  modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,44 +1,56 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-book-table]'),
-    closeModalBtn: document.querySelector('[data-modal-close-book-table]'),
-    modal: document.querySelector('[data-modal-book-table]'),
-  };
+// OPEN MODAL"room-price"
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+$('.js-open-modal').click(function () {
+  var modalName = $(this).attr('data-modal');
+  var modal = $('.js-modal[data-modal="' + modalName + '"]');
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+  modal.addClass('is-show');
+  $('.js-backdrop').addClass('is-show');
+});
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-book-room]'),
-    closeModalBtn: document.querySelector('[data-modal-close-book-room]'),
-    modal: document.querySelector('[data-modal-book-room]'),
-  };
+// CLOSE MODAL - CLICK IN BUTTON(x)
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+$('.js-modal-close').click(function () {
+  $(this).parent('.js-modal').removeClass('is-show');
+  $('.js-backdrop').removeClass('is-show');
+});
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-room-price]'),
-    closeModalBtn: document.querySelector('[data-modal-close-room-price]'),
-    modal: document.querySelector('[data-modal-room-price]'),
-  };
+// $('.js-backdrop').click(function () {
+//   $('.js-modal.is-show').removeClass('is-show');
+//   $(this).removeClass('is-show');
+// });
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+// OPEN MODAL "book-room"
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+$('.js-open-modal1').click(function () {
+  var modalName = $(this).attr('data-modal');
+  var modal = $('.js-modal1[data-modal="' + modalName + '"]');
+
+  modal.addClass('is-show');
+  $('.js-backdrop1').addClass('is-show');
+});
+
+// CLOSE MODAL - CLICK IN BUTTON(x)
+
+$('.js-modal-close1').click(function () {
+  $(this).parent('.js-modal1').removeClass('is-show');
+  $('.js-backdrop1').removeClass('is-show');
+});
+
+// OPEN MODAL "book-table"
+
+$('.js-open-modal2').click(function () {
+  var modalName = $(this).attr('data-modal');
+  var modal = $('.js-modal2[data-modal="' + modalName + '"]');
+
+  modal.addClass('is-show');
+  $('.js-backdrop2').addClass('is-show');
+});
+
+// CLOSE MODAL - CLICK IN BUTTON(x)
+
+$('.js-modal-close2').click(function () {
+  $(this).parent('.js-modal2').removeClass('is-show');
+  $('.js-backdrop2').removeClass('is-show');
+});
